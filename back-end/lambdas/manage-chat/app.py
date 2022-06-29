874639e027930a8970252755edc51abb91cc4415
Upen <upen.dhakal@changehealthcare.com>
2022-06-28 21:36:29 -0500

    organize endpoints better

--- back-end/lambdas/manage-chat/app.py
+++ back-end/lambdas/manage-chat/app.py
@@ -1,21 +1,21 @@
-from flask import Flask, jsonify, make_response
-
-app = Flask(__name__)
-
+from ensurepip import version
+from flask import Flask, jsonify, make_response, request
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_from_root():
-    return jsonify(message='Hello from root!')
+def general_info():
+    return jsonify(api='manage-chat', app='me', version='1.0')
 
 
-@app.route("/hello")
-def hello():
-    return jsonify(message='Hello from path!')
+@app.route("/response")
+def get_response():
+    args = request.args
+    query = args.get("q")
+    return jsonify(question=query, response='Oops! I cannot understand that.')
 
 
 @app.errorhandler(404)
 def resource_not_found(e):
-    return make_response(jsonify(error='Not found!'), 404)
+    return make_response(jsonify(error='The api does not exist for this path'), 404)
